8dee29587c90de1be4fd6a36f162209edc482d12
juhui <juya202106@naver.com>
2025-06-04 09:51:24 +0900

    2025/06/04 9시수업

---
+++ 12_JUHUI/jpa-lecture/chap04-jpql-lecture-source/src/main/java/com/ohgiraffers/jpql/section03/projection/EmbeddedMenu.java
@@ -0,0 +1,10 @@
+package com.ohgiraffers.jpql.section03.projection;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tbl_menu")
+public class EmbeddedMenu {
+
+}
