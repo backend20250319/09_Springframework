9fe9034374afc956e773a4317640ae0415eb2339
parkchangjun <parkchangjun13@gmail.com>
2025-06-04 17:55:15 +0900

    chap06-spring-data-jpa commit

---
+++ 06_PARKCHANGJUN/jpa-lecture/chap06-spring-data-jpa-lecture-source/src/main/java/com/ohgiraffers/datajpa/menu/controller/MenuController.java
@@ -0,0 +1,11 @@
+package com.ohgiraffers.datajpa.menu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// Controller 어노테이션이 없으면 RequestMapping은 작동하지 않는다.
+@Controller
+@RequestMapping("/menu")
+public class MenuController {
+
+}
---
+++ 06_PARKCHANGJUN/jpa-lecture/chap06-spring-data-jpa-lecture-source/src/main/java/com/ohgiraffers/datajpa/menu/service/MenuService.java
@@ -0,0 +1,9 @@
+package com.ohgiraffers.datajpa.menu.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MenuService {
+
+
+}
