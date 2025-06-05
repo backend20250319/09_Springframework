c36191d1468e33aafe1aa11db59f5999fc93dcba
jungho <kiik52@naver.com>
2025-06-05 14:40:51 +0900

    chap01-rest-api - Response   - 4. map 응답

--- 20_JIJUNGHO/rest-api-lecture/chap01-rest-api-lecture-source/src/main/java/ohgiraffers/restapi/section01/response/ResponseController.java
+++ 20_JIJUNGHO/rest-api-lecture/chap01-rest-api-lecture-source/src/main/java/ohgiraffers/restapi/section01/response/ResponseController.java
@@ -4,7 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /* @RestController : 모든 핸들러 메소드에 @Responese가 적용된것과 같다. */
 @RestController
@@ -25,7 +29,19 @@ public class ResponseController {
     /* 3. List 응답 */
     @GetMapping("/list")
     public List<String> getList() {
-        return List.of(new String[]{"사과","바나나","복숭아","수박"});
+        return List.of(new String[]{"사과", "바나나", "복숭아", "수박"});
+    }
+
+    /* 4. map 응답 */
+    @GetMapping("/map")
+    public Map<Integer, String> getMap() {
+        List<Message> messageList = new ArrayList<>();
+
+        messageList.add(new Message(200, "정상 응답!"));
+        messageList.add(new Message(404, "페이지를 찾을 수 없음"));
+        messageList.add(new Message(500, "개발자의 잘못 입니다."));
+
+        return messageList.stream().collect(Collectors.toMap(Message::getHttpStatusCode, Message::getMessage));
     }
 
 }
