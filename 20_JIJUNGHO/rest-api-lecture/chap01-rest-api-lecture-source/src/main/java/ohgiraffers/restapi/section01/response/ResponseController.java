5d66a38a7489345b3c1a2b070625f2353b4b9d4a
jungho <kiik52@naver.com>
2025-06-05 14:33:41 +0900

    chap01-rest-api - Response   - 3. List 응답

--- 20_JIJUNGHO/rest-api-lecture/chap01-rest-api-lecture-source/src/main/java/ohgiraffers/restapi/section01/response/ResponseController.java
+++ 20_JIJUNGHO/rest-api-lecture/chap01-rest-api-lecture-source/src/main/java/ohgiraffers/restapi/section01/response/ResponseController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /* @RestController : 모든 핸들러 메소드에 @Responese가 적용된것과 같다. */
 @RestController
 @RequestMapping("/response")
@@ -16,8 +18,14 @@ public class ResponseController {
 
     /* 2. Object 응답 */
     @GetMapping("/message")
-    public Message message() {
+    public Message getMessage() {
         return new Message(200, "메세지를 응답합니다.");
+    }
+
+    /* 3. List 응답 */
+    @GetMapping("/list")
+    public List<String> getList() {
+        return List.of(new String[]{"사과","바나나","복숭아","수박"});
     }
 
 }
