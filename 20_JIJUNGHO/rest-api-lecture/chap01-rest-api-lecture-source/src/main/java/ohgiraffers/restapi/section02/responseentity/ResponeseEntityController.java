4575f5840fc2e21a9cf86e05e22db50ab4094ab2
jungho <kiik52@naver.com>
2025-06-05 15:39:14 +0900

    chap01-rest-api - ResponseEntity   - 회원 번호 조회

--- 20_JIJUNGHO/rest-api-lecture/chap01-rest-api-lecture-source/src/main/java/ohgiraffers/restapi/section02/responseentity/ResponeseEntityController.java
+++ 20_JIJUNGHO/rest-api-lecture/chap01-rest-api-lecture-source/src/main/java/ohgiraffers/restapi/section02/responseentity/ResponeseEntityController.java
@@ -6,6 +6,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,14 +21,14 @@ public class ResponeseEntityController {
 
     public ResponeseEntityController() {
         users = new ArrayList<>();
-        users.add(new UserDTO(1, "user01", "pass01","유관순"));
-        users.add(new UserDTO(2, "user02", "pass02","홍길동"));
-        users.add(new UserDTO(3, "user03", "pass03","이순신"));
+        users.add(new UserDTO(1, "user01", "pass01", "유관순"));
+        users.add(new UserDTO(2, "user02", "pass02", "홍길동"));
+        users.add(new UserDTO(3, "user03", "pass03", "이순신"));
 
     }
 
     @GetMapping("/user")
-    public ResponseEntity<ResponeseMessage> findAllUser(){
+    public ResponseEntity<ResponeseMessage> findAllUser() {
         /* 응답 헤더 설정 : JSON 응답이 default 이기는 하나 변경이 필요한 경우 HttpHeaders 설정 변경 */
         HttpHeaders httpHeaders = new HttpHeaders();
 
@@ -45,8 +46,38 @@ public class ResponeseEntityController {
         );
 
         return new ResponseEntity<>(responeseMessage, httpHeaders, HttpStatus.OK);
-
     }
 
-   
+    @GetMapping("/users/{userNo}")
+    public ResponseEntity<ResponeseMessage> findUserByNo(@PathVariable int userNo) {
+        /* 응답 헤더 설정 : JSON 응답이 default 이기는 하나 변경이 필요한 경우 HttpHeaders 설정 변경 */
+        HttpHeaders httpHeaders = new HttpHeaders();
+
+        httpHeaders.setContentType(
+                new MediaType("application", "json", StandardCharsets.UTF_8)
+        );
+
+        /* 응답에 대한 body 설정 */
+        Map<String, Object> responseMap = new HashMap<>();
+        UserDTO foundUser = users.stream().filter(user -> user.getNo() == userNo).findFirst().get();
+        responseMap.put("foundUser", foundUser);
+
+        /* 응답 메세지 설정 */
+        ResponeseMessage responeseMessage = new ResponeseMessage(
+                200, "조회 성공", responseMap
+        );
+
+        return new ResponseEntity<>(responeseMessage, httpHeaders, HttpStatus.OK);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<ResponeseMessage> registUser(@RequestBody UserDTO userDTO) {
+
+        int lastUserNo = users.get(users.size() - 1).getNo();
+        userDTO.setNo(lastUserNo + 1);
+        users.add(userDTO);
+
+        return ResponseEntity.created(URI.create("/entity/users" + users.get(users.size() - 1).getNo())).build();
+    }
+
 }
