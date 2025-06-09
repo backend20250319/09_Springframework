93a9ecbe3bd2a1cb100446d1b9bb182f843447b6
juhui <juya202106@naver.com>
2025-06-09 17:50:19 +0900

    06/09 chap03 spring security까지

---
+++ 00_YUSEUNGJE/rest-api-lecture/99_jwt-encode-decode/src/com/ohgiraffers/base64/EncodingDecoding.java
@@ -0,0 +1,25 @@
+package com.ohgiraffers.base64;
+
+import java.util.Base64;
+
+public class EncodingDecoding {
+
+    public static void main(String[] args) {
+
+        // Java 8에서 제공하는 기본 Base64 Encoder 와 Decoder
+        Base64.Encoder encoder = Base64.getEncoder();
+        Base64.Decoder decoder = Base64.getDecoder();
+
+        /* encode */
+        String testStr = "base64로인코딩한비밀키";
+        byte[] encodeArr = testStr.getBytes();
+
+        byte[] encodeByte = encoder.encode(encodeArr);
+        String encodeStr = new String(encodeByte);
+        System.out.println("encodeStr = " + encodeStr);
+
+        /* decode */
+        byte[] decodeByte = decoder.decode(encodeStr);
+        System.out.println("decodeByte = " + new String(decodeByte));
+    }
+}
