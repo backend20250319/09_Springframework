a0ad121861fefbf2c0727a291610c4c724c7db49
juhui <juya202106@naver.com>
2025-06-09 13:58:29 +0900

    추가

--- 12_JUHUI/rest-api-lecture/chap02-cqrs-lecture-source/src/main/java/com/ohgiraffers/cqrs/products/command/domain/aggregate/Product.java
+++ 12_JUHUI/rest-api-lecture/chap02-cqrs-lecture-source/src/main/java/com/ohgiraffers/cqrs/products/command/domain/aggregate/Product.java
@@ -7,7 +7,6 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
