3d97235b11519b5e0ab1d9bb38b6e44fae081894
dkrio <dkrio34@gmail.com>
2025-06-09 17:51:36 +0900

    0609 수업 내용 commit

---
+++ 18_JUNGSEONGWON/rest-api-lecture/chap02-cqrs-lecture-resoure/src/main/java/com/jsw/cqrs/Chap02CqrsLectureResoureApplication.java
@@ -0,0 +1,13 @@
+package com.jsw.cqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap02CqrsLectureResoureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap02CqrsLectureResoureApplication.class, args);
+    }
+
+}
---
+++ 18_JUNGSEONGWON/rest-api-lecture/chap02-cqrs-lecture-resoure/src/main/java/com/jsw/cqrs/products/query/dto/response/ProductListResponse.java
@@ -0,0 +1,15 @@
+package com.jsw.cqrs.products.query.dto.response;
+
+import com.jsw.cqrs.common.dto.Pagination;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class ProductListResponse {
+
+    private final List<ProductDto> products;
+    private final Pagination pagination;
+}
