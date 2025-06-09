ef8a395690dd7a6bfc942f32b41f7758654ff426
juhui <juya202106@naver.com>
2025-06-09 20:21:56 +0900

    swagger부분인 section05 부분에서 클래스를 java가 아닌 kotlin으로 생성된 문제점 발견, 해결완료

--- 12_JUHUI/rest-api-lecture/chap01-rest-api-lecture-source/src/main/java/com/ohgiraffers/restapi/Chap01RestApiLectureSourceApplication.java
+++ 12_JUHUI/rest-api-lecture/chap01-rest-api-lecture-source/src/main/java/com/ohgiraffers/restapi/Chap01RestApiLectureSourceApplication.java
@@ -2,8 +2,10 @@ package com.ohgiraffers.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.ohgiraffers.restapi")
 public class Chap01RestApiLectureSourceApplication {
 
     public static void main(String[] args) {
