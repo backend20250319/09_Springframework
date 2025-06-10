38e800331999551e4523df56549325536f98aaf4
jungho <kiik52@naver.com>
2025-06-10 12:14:31 +0900

    chap03 - spring-security - accesstoken, refreshtoken 생성 메서드 구현 - application.yml 파일에 jwt 토큰 설정 - 회원가입한 정보로 로그인 구현

--- 20_JIJUNGHO/rest-api-lecture/chap03-spring-security-lecture-source/src/main/java/com/ohgiraffers/springsecurity/command/repository/UserRepository.java
+++ 20_JIJUNGHO/rest-api-lecture/chap03-spring-security-lecture-source/src/main/java/com/ohgiraffers/springsecurity/command/repository/UserRepository.java
@@ -2,10 +2,11 @@ package com.ohgiraffers.springsecurity.command.repository;
 
 import com.ohgiraffers.springsecurity.command.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByUsername(String username);
 
 }
